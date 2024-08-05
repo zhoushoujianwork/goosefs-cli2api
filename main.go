d64de176c324b1f9331c7bf213e2e0d51bc4c9d5
zhoushoujian <zhoushoujianwork@163.com>
2024-08-05 15:23:01 +0800

    add version

--- main.go
+++ main.go
@@ -5,11 +5,13 @@ package main
 
 import "goosefs-cli2api/cmd"
 
+var version string
+
 // @title           GooseFS-CLI2API
 // @version         v1
 // @termsOfService  http://swagger.io/terms/
 // @host            localhost:8080
 // @BasePath
 func main() {
-	cmd.Execute()
+	cmd.Execute(version)
 }
